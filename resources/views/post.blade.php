cfc08227cdaf847fb854a0f66c208ed12926f70d
ahmedasad236 <ahmedasad236@gmail.com>
2022-05-14 10:07:48 +0200

    Add First Laravel Example

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!docktype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<!-- <script src="/app.js"></script> -->
+<body>
+    
+    <?= $post; ?>
+    <a href="/">Go Back</a>
+    
+</body>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<!docktype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<script src="/app.js"></script>
+<body>Hello World</body>
